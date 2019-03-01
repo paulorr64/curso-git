7c7d2849e633c57c0f7dde8ef7946976aad6cf7f
Paulo Roberto <paulorr64@gmail.com>
2019-02-28 23:34:55 -0300

    Projeto iniciado, mostrando a mensagem Ola Mundo

---
+++ projeto_teste/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace projeto_teste {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine();
+        }
+    }
+}
